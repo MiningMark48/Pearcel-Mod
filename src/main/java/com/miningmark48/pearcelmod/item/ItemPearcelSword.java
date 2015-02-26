82a955b8393d3599fef95158c8a9da8c620525e2
MiningMark48 <miningmark48@gmail.com>
2015-02-26 18:10:23 -0500

    Speed effect when holding Pearcel Sword

--- src/main/java/com/miningmark48/pearcelmod/item/ItemPearcelSword.java
+++ src/main/java/com/miningmark48/pearcelmod/item/ItemPearcelSword.java
@@ -4,20 +4,40 @@ import java.util.List;
 
 import com.miningmark48.pearcelmod.creativetab.CreativeTabPearcelMod;
 
+import cpw.mods.fml.common.gameevent.PlayerEvent;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemSword;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.world.World;
+import net.minecraftforge.event.entity.living.LivingAttackEvent;
+import net.minecraftforge.event.entity.player.AttackEntityEvent;
 
 public class ItemPearcelSword extends ItemSword{
 
 	public ItemPearcelSword(ToolMaterial material)
     {
 		super(material);
+        //setPotionEffect(Potion.moveSlowdown.id, 15, 1, 0.5F);
 	}
-	
-	@Override
+
+    @Override
 	public void addInformation(ItemStack par1ItemStack, EntityPlayer par2EntityPlayer, List par3List, boolean par4) {
-            par3List.add("A pear of these would be better...");
+            par3List.add("My energy rises when I hold this... ");
     }
-	
+
+    @Override
+    public void onUpdate(ItemStack stack, World world, Entity entity, int par4, boolean par5){
+        super.onUpdate(stack, world, entity, par4, par5);
+        {
+            EntityPlayer player = (EntityPlayer) entity;
+            ItemStack equipped = player.getCurrentEquippedItem();
+            if (equipped == stack){
+                player.addPotionEffect(new PotionEffect(Potion.moveSpeed.getId(), 20, 0));
+            }
+        }
+    }
 }
