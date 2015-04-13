9f38c9b299184870eaced3adc6c9c0cee54b9e91
MiningMark48 <miningmark48@gmail.com>
2015-04-13 19:21:52 -0400

    Melon Sandwich Achievement

--- src/main/java/com/miningmark48/pearcelmod/achievements/Achievements.java
+++ src/main/java/com/miningmark48/pearcelmod/achievements/Achievements.java
@@ -14,6 +14,7 @@ public class Achievements {
     public static Achievement achievementSugarCookie;
     public static Achievement achievementPearcelOre;
     public static Achievement achievementPearcelMelon;
+    public static Achievement achievementMelonSandwich;
 
     public static void init(){
         //Creates a new achievement
@@ -21,15 +22,17 @@ public class Achievements {
         achievementSugarCookie = new Achievement("achievement.craftSugarCookie", "craftSugarCookie", 0, 2, new ItemStack(ModItems.sugarCookie), (Achievement)null).initIndependentStat().registerStat();
         achievementPearcelOre = new Achievement("achievement.findPearcelOre", "findPearcelOre", 2, 0, new ItemStack(ModBlocks.pearcelOre), (Achievement)null).initIndependentStat().registerStat();
         achievementPearcelMelon = new Achievement("achievement.craftPearcelMelon", "craftPearcelMelon", 2, 2, new ItemStack(ModItems.pearcelMelon), (Achievement)null).initIndependentStat().registerStat();
+        achievementMelonSandwich = new Achievement("achievement.craftMelonSandwich", "craftMelonSandwich", 4, 2, new ItemStack(ModItems.melonSandwich), (Achievement)null).initIndependentStat().registerStat();
 
         //Creates Achievement Page
-        AchievementPage.registerAchievementPage(new AchievementPage("Pearcel Mod", new Achievement[]{achievementPearcelSword, achievementSugarCookie, achievementPearcelOre, achievementPearcelMelon}));
+        AchievementPage.registerAchievementPage(new AchievementPage("Pearcel Mod", new Achievement[]{achievementPearcelSword, achievementSugarCookie, achievementPearcelOre, achievementPearcelMelon, achievementMelonSandwich}));
 
         //Register Achievements
         FMLCommonHandler.instance().bus().register(new CraftPearcelSword());
         FMLCommonHandler.instance().bus().register(new CraftSugarCookie());
         FMLCommonHandler.instance().bus().register(new FindPearcelOre());
         FMLCommonHandler.instance().bus().register(new CraftPearcelMelon());
+        FMLCommonHandler.instance().bus().register(new CraftMelonSandwich());
 
         LogHelper.info("Achievements loaded!");
     }
