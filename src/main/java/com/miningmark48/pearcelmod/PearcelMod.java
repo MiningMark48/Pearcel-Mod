e8a5960a0576055fbc8a37a0e04f69265e99c9be
MiningMark48 <miningmark48@gmail.com>
2015-05-05 21:44:24 -0400

    Removed some un-needed code, possible bug fixes

--- src/main/java/com/miningmark48/pearcelmod/PearcelMod.java
+++ src/main/java/com/miningmark48/pearcelmod/PearcelMod.java
@@ -2,12 +2,9 @@ package com.miningmark48.pearcelmod;
 
 import com.miningmark48.pearcelmod.achievements.Achievements;
 import com.miningmark48.pearcelmod.client.gui.GuiHandler;
-import com.miningmark48.pearcelmod.client.handler.KeyInputEventHandler;
 import com.miningmark48.pearcelmod.event.EventFillBucket;
 import com.miningmark48.pearcelmod.handler.ConfigurationHandler;
 import com.miningmark48.pearcelmod.init.*;
-import com.miningmark48.pearcelmod.proxy.ClientProxy;
-import com.miningmark48.pearcelmod.proxy.CommonProxy;
 import com.miningmark48.pearcelmod.proxy.IProxy;
 
 import com.miningmark48.pearcelmod.reference.Reference;
@@ -22,11 +19,7 @@ import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.event.FMLServerStartingEvent;
 import cpw.mods.fml.common.network.NetworkRegistry;
 import cpw.mods.fml.common.registry.GameRegistry;
-import net.minecraft.init.Items;
-import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fluids.FluidContainerRegistry;
-import net.minecraftforge.fluids.FluidRegistry;
 
 @Mod(modid=Reference.MOD_ID, name=Reference.MOD_NAME, version=Reference.VERSION, guiFactory = Reference.GUI_FACTORY_CLASS)
 public class PearcelMod {
@@ -60,9 +53,7 @@ public class PearcelMod {
 	
 	@Mod.EventHandler
 	public void init(FMLInitializationEvent event){
-        FMLCommonHandler.instance().bus().register(new KeyInputEventHandler());
         MinecraftForge.EVENT_BUS.register(new EventFillBucket());
-        //FMLCommonHandler.instance().bus().register(new KeyInputEventHandler());
         Recipes.init();
         Achievements.init();
         LogHelper.info("Init Complete!");
--- src/main/java/com/miningmark48/pearcelmod/client/settings/Keybindings.java
+++ src/main/java/com/miningmark48/pearcelmod/client/settings/Keybindings.java
@@ -6,7 +6,5 @@ import org.lwjgl.input.Keyboard;
 
 public class Keybindings {
 
-    public static KeyBinding regen = new KeyBinding(Names.Keys.REGEN, Keyboard.KEY_R, Names.Keys.CATEGORY);
-    public static KeyBinding clear = new KeyBinding(Names.Keys.CLEAR, Keyboard.KEY_C, Names.Keys.CATEGORY);
 
 }
