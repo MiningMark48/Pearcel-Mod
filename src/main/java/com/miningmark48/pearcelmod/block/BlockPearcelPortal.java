6748d55ef3ea679569a46e4cff6ac88f86c0ce47
MiningMark48 <miningmark48@gmail.com>
2015-06-22 22:40:18 -0400

    More Pearcel Dimension work

--- src/main/java/com/miningmark48/pearcelmod/block/BlockPearcelPortal.java
+++ src/main/java/com/miningmark48/pearcelmod/block/BlockPearcelPortal.java
@@ -1,11 +1,19 @@
 package com.miningmark48.pearcelmod.block;
 
+import com.miningmark48.pearcelmod.dimension.DimensionRegistry;
+import com.miningmark48.pearcelmod.dimension.TeleporterPearcel;
 import com.miningmark48.pearcelmod.init.ModBlocks;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockPortal;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.init.Blocks;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.AxisAlignedBB;
+import net.minecraft.world.Teleporter;
 import net.minecraft.world.World;
 
+import java.awt.*;
 import java.util.Random;
 
 public class BlockPearcelPortal extends BlockPortal {
@@ -63,6 +71,71 @@ public class BlockPearcelPortal extends BlockPortal {
             }else{
                 world.setBlockToAir(x, y, z);
             }
+        }
+    }
+
+    public int quantityDropped(Random rand){
+        return 0;
+    }
+
+    public void onEntityCollideWithBlock(World world, int x, int y, int z, Entity entity){
+        if (entity.ridingEntity == null && entity.riddenByEntity == null && entity instanceof EntityPlayerMP){
+            EntityPlayerMP thePlayer = (EntityPlayerMP) entity;
+
+            MinecraftServer server = MinecraftServer.getServer();
+            if (entity instanceof EntityPlayerMP){
+                if (thePlayer.timeUntilPortal > 0){
+                    thePlayer.timeUntilPortal = 10;
+                }else if(thePlayer.dimension != DimensionRegistry.dimensionID){
+                    thePlayer.timeUntilPortal = 10;
+                    thePlayer.mcServer.getConfigurationManager().transferPlayerToDimension(thePlayer, DimensionRegistry.dimensionID, new TeleporterPearcel(server.worldServerForDimension(DimensionRegistry.dimensionID)));
+                }else{
+                    thePlayer.timeUntilPortal = 10;
+                    thePlayer.mcServer.getConfigurationManager().transferPlayerToDimension(thePlayer, 0, new TeleporterPearcel(server.worldServerForDimension(0)));
+                }
+            }
+        }
+    }
+
+    public boolean tryToCreatePortal(World world, int x, int y, int z){
+        byte b0 = 0;
+        byte b1 = 0;
+        if(world.getBlock(x - 1, y, z) == ModBlocks.pearcelBlock || world.getBlock(x + 1, y, z) == ModBlocks.pearcelBlock){
+            b0 = 1;
+        }
+        if(world.getBlock(x, y, z - 1) == ModBlocks.pearcelBlock || world.getBlock(x, y, z + 1) == ModBlocks.pearcelBlock){
+            b1 = 1;
+        }
+        if (b0 == b1){
+            return false;
+        }else{
+            if (world.getBlock(x - b0, y, z - b1) == Blocks.air){
+                x -= b0;
+                z -= b1;
+            }
+            int l;
+            int i1;
+            for (l = -1; l <= 2; ++l){
+                for (i1 = -1; i1 <= 3; ++i1){
+                    boolean flag = l == -1 || l == 2 || i1 == -1 || i1 == 3;
+                    if (l != -1 && l != 2 || i1 != -1 && i1 != 3){
+                        Block j1 = world.getBlock(x + b0 * l, y + i1, z + b1 * l);
+                        if (flag){
+                            if (j1 != ModBlocks.pearcelBlock){
+                                return false;
+                            }
+                        }else if(j1 != Blocks.air && j1 != Blocks.fire){
+                            return false;
+                        }
+                    }
+                }
+            }
+            for (l = 0; l < 2; ++l){
+                for (i1 = 0; i1 < 3; ++i1){
+                    world.setBlock(x + b0 * l, y + i1, z + b1 * l, ModBlocks.pearcelPortal, 0, 2);
+                }
+            }
+            return true;
         }
     }
 }
