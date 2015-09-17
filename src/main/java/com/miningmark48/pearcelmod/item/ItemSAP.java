843d2f25172cdaa8d6fdcb030c57dce3793abd59
MiningMark48 <miningmark48@gmail.com>
2015-09-16 21:09:21 -0400

    tooltip

--- src/main/java/com/miningmark48/pearcelmod/item/ItemSAP.java
+++ src/main/java/com/miningmark48/pearcelmod/item/ItemSAP.java
@@ -1,9 +1,22 @@
 package com.miningmark48.pearcelmod.item;
 
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+
+import java.util.List;
+
 public class ItemSAP extends ItemPearcelMod{
 
     public ItemSAP(){
         super();
+    }
+
+    @Override
+    public void addInformation(ItemStack par1ItemStack, EntityPlayer par2EntityPlayer, List par3List, boolean par4) {
+        par3List.add("Right click upon the Summoner ");
+        par3List.add("to activate the dark Pearcel magic, ");
+        par3List.add("in which a creature will spawn ");
+        par3List.add("from the depths of the surrounding summoner blocks.");
     }
 
 }
