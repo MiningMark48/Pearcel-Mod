2f003d15ef37bc5e6225ab54685ffcb5a53065b1
Mark <miningmark48@gmail.com>
2015-10-26 23:30:00 -0400

    Added Tooltip

--- src/main/java/com/miningmark48/pearcelmod/item/ItemFlight.java
+++ src/main/java/com/miningmark48/pearcelmod/item/ItemFlight.java
@@ -1,8 +1,18 @@
 package com.miningmark48.pearcelmod.item;
 
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+
+import java.util.List;
+
 public class ItemFlight extends ItemPearcelMod{
 
     public ItemFlight(){
         super();
+    }
+
+    @Override
+    public void addInformation(ItemStack par1ItemStack, EntityPlayer par2EntityPlayer, List par3List, boolean par4) {
+        par3List.add("Fly in the air on a whispy cloud.");
     }
 }
