5bcb212825e7584c45cc89e27379e8febfc55e2d
Mark <miningmark48@gmail.com>
2015-11-28 19:12:22 -0500

    removed unused import

--- src/main/java/com/miningmark48/pearcelmod/item/ItemFlight.java
+++ src/main/java/com/miningmark48/pearcelmod/item/ItemFlight.java
@@ -1,13 +1,8 @@
 package com.miningmark48.pearcelmod.item;
 
-import com.miningmark48.pearcelmod.handler.ConfigurationHandler;
-import com.miningmark48.pearcelmod.init.ModItems;
-import com.miningmark48.pearcelmod.utility.LogHelper;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.ChatComponentTranslation;
 import net.minecraft.util.StatCollector;
-import net.minecraft.world.World;
 
 import java.util.List;
 
