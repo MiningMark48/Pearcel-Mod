7ee2f94f8f8a0a499782e8e616709f20fed9f54e
Mark <miningmark48@gmail.com>
2015-11-28 19:08:06 -0500

    stuff

--- src/main/java/com/miningmark48/pearcelmod/item/ItemFlight.java
+++ src/main/java/com/miningmark48/pearcelmod/item/ItemFlight.java
@@ -1,5 +1,6 @@
 package com.miningmark48.pearcelmod.item;
 
+import com.miningmark48.pearcelmod.handler.ConfigurationHandler;
 import com.miningmark48.pearcelmod.init.ModItems;
 import com.miningmark48.pearcelmod.utility.LogHelper;
 import net.minecraft.entity.player.EntityPlayer;
