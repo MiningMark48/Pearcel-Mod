b745da51bd0fe988693972463384986ec9743b08
Mark <miningmark48@gmail.com>
2016-01-30 18:04:11 -0500

    Tooltip

--- src/main/java/com/miningmark48/pearcelmod/item/ItemPearcelMatterNeutral.java
+++ src/main/java/com/miningmark48/pearcelmod/item/ItemPearcelMatterNeutral.java
@@ -1,9 +1,21 @@
 package com.miningmark48.pearcelmod.item;
 
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumChatFormatting;
+import net.minecraft.util.StatCollector;
+
+import java.util.List;
+
 public class ItemPearcelMatterNeutral extends ItemPearcelMod{
 
     public ItemPearcelMatterNeutral(){
         super();
+    }
+
+    @Override
+    public void addInformation(ItemStack par1ItemStack, EntityPlayer par2EntityPlayer, List par3List, boolean par4) {
+        par3List.add(StatCollector.translateToLocal(EnumChatFormatting.YELLOW + "tooltip.item.pearcelMatter.line1"));
     }
 
 }
