c2ea5eb9c895360fd7ecb6c215b2f0ec467f4a03
Mark <miningmark48@gmail.com>
2016-03-16 17:56:03 -0400

    pearcel cloud tempoararily disabled

--- src/main/java/com/miningmark48/pearcelmod/item/ItemFlight.java
+++ src/main/java/com/miningmark48/pearcelmod/item/ItemFlight.java
@@ -1,10 +1,14 @@
 package com.miningmark48.pearcelmod.item;
 
 import com.miningmark48.pearcelmod.handler.ConfigurationHandler;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
 import net.minecraft.util.EnumChatFormatting;
 import net.minecraft.util.StatCollector;
+import net.minecraft.world.World;
 
 import java.util.List;
 
@@ -17,7 +21,7 @@ public class ItemFlight extends ItemPearcelMod{
     @Override
     public void addInformation(ItemStack par1ItemStack, EntityPlayer par2EntityPlayer, List par3List, boolean par4) {
         if (!ConfigurationHandler.flightItemDisable) {
-            par3List.add(StatCollector.translateToLocal("tooltip.item.itemFlight.line1"));
+            par3List.add(StatCollector.translateToLocal("tooltip.item.itemFlight.line1.temp"));
         }else{
             par3List.add(EnumChatFormatting.DARK_RED + StatCollector.translateToLocal("tooltip.item.itemFlight.disabled"));
         }
