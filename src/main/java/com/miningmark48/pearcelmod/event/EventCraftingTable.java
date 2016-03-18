0098f87b1da7c5eb30e24149b77fd2fdac09e41d
Mark <miningmark48@gmail.com>
2016-03-18 19:07:51 -0400

    config stuff

--- src/main/java/com/miningmark48/pearcelmod/event/EventCraftingTable.java
+++ src/main/java/com/miningmark48/pearcelmod/event/EventCraftingTable.java
@@ -1,6 +1,7 @@
 package com.miningmark48.pearcelmod.event;
 
 import com.miningmark48.pearcelmod.handler.ConfigurationHandler;
+import com.miningmark48.pearcelmod.init.ModBlocks;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import net.minecraft.init.Blocks;
 import net.minecraft.util.ChatComponentTranslation;
@@ -14,7 +15,7 @@ public class EventCraftingTable {
     public void playerInteract(PlayerInteractEvent event){
         if (ConfigurationHandler.craftingTables) {
             if (event.action.equals(PlayerInteractEvent.Action.RIGHT_CLICK_BLOCK)) {
-                if (event.world.getBlock(event.x, event.y, event.z).equals(Blocks.crafting_table)) {
+                if (event.world.getBlock(event.x, event.y, event.z).equals(Blocks.crafting_table) || event.world.getBlock(event.x, event.y, event.z).equals(ModBlocks.pearcelWorkbench)) {
                     event.setCanceled(true);
                     event.entityPlayer.addChatComponentMessage(new ChatComponentTranslation(EnumChatFormatting.DARK_RED  + StatCollector.translateToLocal("chat.event.noWorkbench")));
                 }
