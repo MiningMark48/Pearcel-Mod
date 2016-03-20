552532946ac2ea69ca5f4b36ffdd343812f569a3
Mark <miningmark48@gmail.com>
2016-03-20 14:34:31 -0400

    more things for the ore dictionary

--- src/main/java/com/miningmark48/pearcelmod/init/OreDict.java
+++ src/main/java/com/miningmark48/pearcelmod/init/OreDict.java
@@ -9,6 +9,10 @@ public class OreDict {
         OreDictionary.registerOre("plankWood", new ItemStack(ModBlocks.pearcelPlanks));
         OreDictionary.registerOre("stickWood", new ItemStack(ModItems.pearcelStick));
         OreDictionary.registerOre("logWood", new ItemStack(ModBlocks.blockLog));
+        OreDictionary.registerOre("treeLeaves", new ItemStack(ModBlocks.blockLeaf));
+        OreDictionary.registerOre("blockGlass", new ItemStack(ModBlocks.pearcelGlass));
+        OreDictionary.registerOre("paneGlass", new ItemStack(ModBlocks.pearcelGlassPane));
+        OreDictionary.registerOre("blockEndStone", new ItemStack(ModBlocks.pearcelEndStone));
     }
 
 }
