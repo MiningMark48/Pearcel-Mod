93a2faaf45e9d1593736d29a3cc01876a2606980
Mark <miningmark48@gmail.com>
2016-03-29 21:39:03 -0400

    testing

--- src/main/java/com/miningmark48/pearcelmod/item/ItemPearcelPickaxe.java
+++ src/main/java/com/miningmark48/pearcelmod/item/ItemPearcelPickaxe.java
@@ -2,6 +2,7 @@ package com.miningmark48.pearcelmod.item;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemPickaxe;
 import net.minecraft.item.ItemStack;
 import net.minecraft.potion.Potion;
@@ -21,5 +22,4 @@ public class ItemPearcelPickaxe extends ItemPickaxe{
     public void addInformation(ItemStack par1ItemStack, EntityPlayer par2EntityPlayer, List par3List, boolean par4) {
         par3List.add(StatCollector.translateToLocal("tooltip.item.pearcelPickaxe.line1"));
     }
-
 }
