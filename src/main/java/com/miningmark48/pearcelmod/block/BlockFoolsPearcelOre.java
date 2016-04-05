0e419ce87bc5a82fec6d86ac207ec9dc144a0bf3
Mark <miningmark48@gmail.com>
2016-04-04 22:55:50 -0400

    Fools Pearcel has a chance of exploding now.

--- src/main/java/com/miningmark48/pearcelmod/block/BlockFoolsPearcelOre.java
+++ src/main/java/com/miningmark48/pearcelmod/block/BlockFoolsPearcelOre.java
@@ -1,5 +1,7 @@
 package com.miningmark48.pearcelmod.block;
 
+import com.miningmark48.pearcelmod.init.ModBlocks;
+import com.miningmark48.pearcelmod.init.ModItems;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
@@ -16,25 +18,33 @@ public class BlockFoolsPearcelOre extends BlockPearcelMod{
 
     @Override
     public void onBlockDestroyedByPlayer(World world, int x, int y, int z, int meta) {
+        Random rand = new Random();
+        int randNum = rand.nextInt(4) + 1;
         if (!world.isRemote){
-            if(world.getBlock(x, y, z) == Blocks.air){
-                world.createExplosion(null, ((double) x), ((double) y), ((double) z), 3F, true);
+            if (randNum == 1) {
+                if (world.getBlock(x, y, z) == Blocks.air) {
+                    world.createExplosion(null, ((double) x), ((double) y), ((double) z), 3F, true);
+                }
             }
         }
     }
 
     @Override
     public void onBlockDestroyedByExplosion(World world, int x, int y, int z, Explosion explosion) {
-        if (!world.isRemote) {
-            if (world.getBlock(x, y, z) == Blocks.air) {
-                world.createExplosion(null, ((double) x), ((double) y), ((double) z), 3F, true);
+        Random rand = new Random();
+        int randNum = rand.nextInt(4) + 1;
+        if (!world.isRemote){
+            if (randNum == 1) {
+                if (world.getBlock(x, y, z) == Blocks.air) {
+                    world.createExplosion(null, ((double) x), ((double) y), ((double) z), 3F, true);
+                }
             }
         }
     }
 
     @Override
     public Item getItemDropped(int meta, Random random, int fortune) {
-        return null;
+        return ModItems.pearcelIngot;
     }
 
 
