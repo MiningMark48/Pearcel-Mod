4c563c3cd0e62617930200a2cc891ddb7b362bf3
MiningMark48 <miningmark48@gmail.com>
2016-04-10 13:59:19 -0400

    If you wear full pearcel armor, you get strength

--- src/main/java/com/miningmark48/pearcelmod/item/ItemPearcelArmor.java
+++ src/main/java/com/miningmark48/pearcelmod/item/ItemPearcelArmor.java
@@ -3,8 +3,12 @@ package com.miningmark48.pearcelmod.item;
 import com.miningmark48.pearcelmod.init.ModItems;
 import com.miningmark48.pearcelmod.reference.Reference;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemArmor;
 import net.minecraft.item.ItemStack;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.world.World;
 
 
 public class ItemPearcelArmor extends ItemArmor {
@@ -20,6 +24,25 @@ public class ItemPearcelArmor extends ItemArmor {
             return Reference.MOD_ID + ":textures/armor/pearcelArmor2.png";
         }else{
             return null;
+        }
+    }
+
+    @Override
+    public void onArmorTick(World world, EntityPlayer player, ItemStack stack){
+        /*
+            3 = Helmet
+            2 = Chestplate
+            1 = Leggings
+            0 = Boots
+        */
+        if(player.getCurrentArmor(3) != null && player.getCurrentArmor(2) != null && player.getCurrentArmor(1) != null && player.getCurrentArmor(0) != null){
+            ItemStack helmet = player.getCurrentArmor(3);
+            ItemStack chestplate = player.getCurrentArmor(2);
+            ItemStack leggings = player.getCurrentArmor(1);
+            ItemStack boots = player.getCurrentArmor(0);
+            if (helmet.getItem() == ModItems.pearcelHelmet && chestplate.getItem() == ModItems.pearcelChestplate && leggings.getItem() == ModItems.pearcelLeggings && boots.getItem() == ModItems.pearcelBoots ){
+                player.addPotionEffect(new PotionEffect(Potion.damageBoost.getId(), 20));
+            }
         }
     }
 
