2b02465d486898806a41d541432661e1011c1878
MiningMark48 <miningmark48@gmail.com>
2016-04-11 20:41:56 -0400

    Time to update, earlier than expected. Server crash fix.

--- src/main/java/com/miningmark48/pearcelmod/reference/Reference.java
+++ src/main/java/com/miningmark48/pearcelmod/reference/Reference.java
@@ -4,7 +4,7 @@ public class Reference {
 
     public static final String MOD_ID = "pearcelmod";
     public static final String MOD_NAME = "PearcelMod";
-    public static final String VERSION = "1.7.10-2.0.6";
+    public static final String VERSION = "1.7.10-2.0.7";
     public static final String CLIENT_PROXY_CLASS = "com.miningmark48.pearcelmod.proxy.ClientProxy";
     public static final String SERVER_PROXY_CLASS = "com.miningmark48.pearcelmod.proxy.ServerProxy";
     public static final String GUI_FACTORY_CLASS = "com.miningmark48.pearcelmod.client.gui.GuiFactory";
