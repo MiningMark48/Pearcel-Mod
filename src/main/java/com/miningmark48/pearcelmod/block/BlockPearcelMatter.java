922b774d73cb266890157ee6886f0e39f6dd0adc
MiningMark48 <miningmark48@gmail.com>
2016-04-20 17:39:32 -0400

    Tooltip addition

--- src/main/java/com/miningmark48/pearcelmod/block/BlockPearcelMatter.java
+++ src/main/java/com/miningmark48/pearcelmod/block/BlockPearcelMatter.java
@@ -1,5 +1,9 @@
 package com.miningmark48.pearcelmod.block;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.item.ItemStack;
+
 public class BlockPearcelMatter extends BlockPearcelMod {
 
     public BlockPearcelMatter(){
