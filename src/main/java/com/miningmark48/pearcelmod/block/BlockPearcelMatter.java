e71fe1fbf312edab67d1b59372398d64c98b6262
MiningMark48 <miningmark48@gmail.com>
2016-04-20 20:24:42 -0400

    Update to readme

--- src/main/java/com/miningmark48/pearcelmod/block/BlockPearcelMatter.java
+++ src/main/java/com/miningmark48/pearcelmod/block/BlockPearcelMatter.java
@@ -1,9 +1,5 @@
 package com.miningmark48.pearcelmod.block;
 
-import cpw.mods.fml.relauncher.Side;
-import cpw.mods.fml.relauncher.SideOnly;
-import net.minecraft.item.ItemStack;
-
 public class BlockPearcelMatter extends BlockPearcelMod {
 
     public BlockPearcelMatter(){
