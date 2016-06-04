cc6226a50856a6775aad35302a49c5c728af7e50
MiningMark48 <miningmark48@gmail.com>
2016-06-04 00:22:50 -0400

    some stuff

--- src/main/java/com/miningmark48/pearcelmod/init/ModItems.java
+++ src/main/java/com/miningmark48/pearcelmod/init/ModItems.java
@@ -30,7 +30,7 @@ public class ModItems {
     public static final Item pearcelPie = new ItemModFood(6, 0.3F, false).setUnlocalizedName("pearcelPie").setTextureName(Reference.MOD_ID + ":pearcelPie").setCreativeTab(CreativeTabPearcelMod.PearcelMod_TAB);
     public static final Item pearcelBread = new ItemModFood(8, 0.8F, false).setUnlocalizedName("pearcelBread").setTextureName(Reference.MOD_ID + ":pearcelBread").setCreativeTab(CreativeTabPearcelMod.PearcelMod_TAB);
     public static final Item pearcelSandwich = new ItemModFood(9, 1.1F, true).setUnlocalizedName("pearcelSandwich").setTextureName(Reference.MOD_ID + ":pearcelSandwich").setCreativeTab(CreativeTabPearcelMod.PearcelMod_TAB);
-    public static final Item pearcelBeef = new ItemModFood(9, 0.7F, true).setUnlocalizedName("pearcelBeef").setTextureName(Reference.MOD_ID + ":pearcelBeef").setCreativeTab(CreativeTabPearcelMod.PearcelMod_TAB);
+    public static final Item pearcelBeef = new ItemModFood(1, 0.7F, true).setUnlocalizedName("pearcelBeef").setTextureName(Reference.MOD_ID + ":pearcelBeef").setCreativeTab(CreativeTabPearcelMod.PearcelMod_TAB);
     public static final Item pearcelSteak = new ItemModFood(9, 1.5F, true).setUnlocalizedName("pearcelSteak").setTextureName(Reference.MOD_ID + ":pearcelSteak").setCreativeTab(CreativeTabPearcelMod.PearcelMod_TAB);
     public static final Item pearcelCookie = new ItemModFood(2, 0.1F, false).setUnlocalizedName("pearcelCookie").setTextureName(Reference.MOD_ID + ":pearcelCookie").setCreativeTab(CreativeTabPearcelMod.PearcelMod_TAB);
     public static final Item pearcelJuiceBottle = new ItemPearcelJuiceBottle().setUnlocalizedName("pearcelJuiceBottle").setTextureName(Reference.MOD_ID + ":pearcelJuiceBottle").setCreativeTab(CreativeTabPearcelMod.PearcelMod_TAB);
