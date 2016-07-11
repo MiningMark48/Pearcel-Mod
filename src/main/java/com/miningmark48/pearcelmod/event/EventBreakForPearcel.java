7f36a9e1092f83cd41cccd5332d83c2738bcd7f7
MiningMark48 <miningmark48@gmail.com>
2016-07-11 00:52:48 -0400

    cleanup

--- src/main/java/com/miningmark48/pearcelmod/event/EventBreakForPearcel.java
+++ src/main/java/com/miningmark48/pearcelmod/event/EventBreakForPearcel.java
@@ -11,7 +11,6 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.common.gameevent.PlayerEvent;
 
 import java.util.Random;
 
