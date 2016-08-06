7a1abbb443f5d25c1c9ecf96c2555eec5e0cab12
MiningMark48 <miningmark48@gmail.com>
2016-08-05 23:07:28 -0400

    Tooltips.

--- src/main/java/com/miningmark48/pearcelmod/item/ItemReinforcedPearcelPickaxe.java
+++ src/main/java/com/miningmark48/pearcelmod/item/ItemReinforcedPearcelPickaxe.java
@@ -1,17 +1,33 @@
 package com.miningmark48.pearcelmod.item;
 
 import com.miningmark48.pearcelmod.handler.ConfigurationHandler;
+import com.miningmark48.pearcelmod.utility.KeyCheck;
+import com.miningmark48.pearcelmod.utility.Translate;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemPickaxe;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumParticleTypes;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
+
+import java.util.List;
 
 public class ItemReinforcedPearcelPickaxe extends ItemPickaxe {
     public ItemReinforcedPearcelPickaxe(ToolMaterial material) {
         super(material);
+    }
+
+    @Override
+    public void addInformation(ItemStack par1ItemStack, EntityPlayer par2EntityPlayer, List par3List, boolean par4) {
+        if (KeyCheck.isHoldingShift()) {
+            par3List.add(Translate.toLocal("tooltip.item.reinforced_pearcel_pickaxe.line1"));
+            par3List.add(Translate.toLocal("tooltip.item.reinforced_pearcel_pickaxe.line2"));
+        }else{
+            par3List.add(Translate.toLocal("tooltip.item.hold") + " " + TextFormatting.AQUA + TextFormatting.ITALIC + Translate.toLocal("tooltip.item.shift"));
+        }
     }
 
     @Override
