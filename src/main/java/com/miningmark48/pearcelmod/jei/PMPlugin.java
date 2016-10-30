b19adfaa626151874f440466d31216f16de92ebb
MiningMark48 <miningmark48@gmail.com>
2016-10-30 00:54:30 -0400

    JEI integration!

--- src/main/java/com/miningmark48/pearcelmod/jei/PMPlugin.java
+++ src/main/java/com/miningmark48/pearcelmod/jei/PMPlugin.java
@@ -2,9 +2,13 @@ package com.miningmark48.pearcelmod.jei;
 
 import com.miningmark48.pearcelmod.init.ModBlocks;
 import com.miningmark48.pearcelmod.init.ModItems;
+import com.miningmark48.pearcelmod.utility.Translate;
 import mezz.jei.api.*;
 import mezz.jei.api.ingredients.IModIngredientRegistration;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+
+import java.util.*;
 
 @JEIPlugin
 public class PMPlugin implements IModPlugin{
@@ -22,14 +26,27 @@ public class PMPlugin implements IModPlugin{
     }
 
     @Override
-    public void register(IModRegistry iModRegistry) {
-        jeiHelpers = iModRegistry.getJeiHelpers();
+    public void register(IModRegistry registry) {
+        jeiHelpers = registry.getJeiHelpers();
+
+        List<ItemStack> backpacks = new ArrayList<ItemStack>();
+        backpacks.add(new ItemStack(ModItems.bindle));
+        backpacks.add(new ItemStack(ModItems.pearcel_backpack));
+        backpacks.add(new ItemStack(ModItems.enlarged_pearcel_backpack));
 
         //Blacklist
         jeiHelpers.getItemBlacklist().addItemToBlacklist(new ItemStack(ModBlocks.lit_pearcel_lamp));
 
         //Descriptions
-        iModRegistry.addDescription(new ItemStack(ModItems.pearcel_item), "Drops from leaves and tall grass.");
+        registry.addDescription(new ItemStack(ModItems.pearcel_item), Translate.toLocal("desc.item.pearcel_item"));
+        registry.addDescription(new ItemStack(ModItems.matter_fabricator), Translate.toLocal("desc.item.matter_fabricator"));
+        registry.addDescription(backpacks, Translate.toLocal("desc.item.backpack"));
+        registry.addDescription(new ItemStack(ModBlocks.torcher), Translate.toLocal("desc.item.torcher"));
+        registry.addDescription(new ItemStack(ModItems.pearcel_bow), Translate.toLocal("desc.item.pearcel_bow"));
+
+        //Other
+        registry.addRecipeCategoryCraftingItem(new ItemStack(ModBlocks.pearcel_workbench), "minecraft.crafting");
+        registry.addRecipeCategoryCraftingItem(new ItemStack(ModItems.pcp), "minecraft.crafting");
 
     }
 
