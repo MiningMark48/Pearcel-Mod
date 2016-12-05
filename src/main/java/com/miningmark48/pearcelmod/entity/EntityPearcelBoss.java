be13ce73e657410b24d26ffcdae2ea49d6e65a7e
MiningMark48 <miningmark48@gmail.com>
2016-12-05 02:15:40 -0500

    New boss mob is under way

--- src/main/java/com/miningmark48/pearcelmod/entity/EntityPearcelBoss.java
+++ src/main/java/com/miningmark48/pearcelmod/entity/EntityPearcelBoss.java
@@ -1,5 +1,6 @@
 package com.miningmark48.pearcelmod.entity;
 
+import com.miningmark48.pearcelmod.init.ModEntities;
 import com.miningmark48.pearcelmod.reference.Reference;
 import net.minecraft.block.Block;
 import net.minecraft.enchantment.EnchantmentHelper;
@@ -12,7 +13,12 @@ import net.minecraft.entity.passive.EntityPig;
 import net.minecraft.entity.passive.EntityVillager;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.init.Items;
+import net.minecraft.init.MobEffects;
 import net.minecraft.init.SoundEvents;
+import net.minecraft.item.ItemAxe;
+import net.minecraft.item.ItemStack;
+import net.minecraft.potion.PotionEffect;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
@@ -20,12 +26,14 @@ import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.BossInfo;
 import net.minecraft.world.BossInfoServer;
 import net.minecraft.world.World;
+import net.minecraft.world.chunk.Chunk;
+import net.minecraft.world.storage.loot.LootTableList;
 
 public class EntityPearcelBoss extends EntityMob{
 
     private final BossInfoServer bossInfo = (BossInfoServer)(new BossInfoServer(this.getDisplayName(), BossInfo.Color.GREEN, BossInfo.Overlay.PROGRESS)).setDarkenSky(true);
 
-    public static ResourceLocation loot_tabel = new ResourceLocation(Reference.MOD_ID+ ":loot_tables/pearcel_boss.json");
+    public static ResourceLocation loot_tabel = new ResourceLocation(Reference.MOD_ID, "loot_tables/pearcel_boss");
 
     public EntityPearcelBoss(World world) {
         super(world);
@@ -44,10 +52,11 @@ public class EntityPearcelBoss extends EntityMob{
     protected void initEntityAI()
     {
         this.tasks.addTask(0, new EntityAISwimming(this));
-        this.tasks.addTask(2, new EntityAIMoveTowardsRestriction(this, 1.0D));
-        this.tasks.addTask(5, new EntityAIWander(this, 1.0D));
-        this.tasks.addTask(7, new EntityAIWatchClosest(this, EntityPlayer.class, 15.0F));
-        this.tasks.addTask(8, new EntityAILookIdle(this));
+        this.tasks.addTask(2, new EntityAIAttackMelee(this, 1.5D, true));
+        this.tasks.addTask(3, new EntityAIMoveTowardsRestriction(this, 1.0D));
+        this.tasks.addTask(6, new EntityAIWander(this, 1.0D));
+        this.tasks.addTask(9, new EntityAIWatchClosest(this, EntityPlayer.class, 15.0F));
+        this.tasks.addTask(9, new EntityAILookIdle(this));
         this.applyEntityAI();
     }
 
@@ -75,12 +84,6 @@ public class EntityPearcelBoss extends EntityMob{
             super.damageEntity(source, amount);
             this.bossInfo.setPercent(getHealth() / getMaxHealth());
         }
-    }
-
-    @Override
-    public EnumCreatureAttribute getCreatureAttribute()
-    {
-        return EnumCreatureAttribute.UNDEAD;
     }
 
     @Override
@@ -117,9 +120,61 @@ public class EntityPearcelBoss extends EntityMob{
     }
 
     @Override
-    public boolean attackEntityAsMob(Entity entityTarget)
+    public boolean attackEntityAsMob(Entity entityIn)
     {
-        return true;
+        float f = (float)this.getEntityAttribute(SharedMonsterAttributes.ATTACK_DAMAGE).getAttributeValue();
+        int i = 0;
+
+        if (entityIn instanceof EntityLivingBase)
+        {
+            f += EnchantmentHelper.getModifierForCreature(this.getHeldItemMainhand(), ((EntityLivingBase)entityIn).getCreatureAttribute());
+            i += EnchantmentHelper.getKnockbackModifier(this);
+        }
+
+        boolean flag = entityIn.attackEntityFrom(DamageSource.causeMobDamage(this), f);
+
+        if (flag)
+        {
+            if (i > 0 && entityIn instanceof EntityLivingBase)
+            {
+                ((EntityLivingBase)entityIn).knockBack(this, (float)i * 0.5F, (double)MathHelper.sin(this.rotationYaw * 0.017453292F), (double)(-MathHelper.cos(this.rotationYaw * 0.017453292F)));
+                this.motionX *= 0.6D;
+                this.motionZ *= 0.6D;
+            }
+
+            int j = EnchantmentHelper.getFireAspectModifier(this);
+
+            if (j > 0)
+            {
+                entityIn.setFire(j * 4);
+            }
+
+            if (entityIn instanceof EntityPlayer)
+            {
+                EntityPlayer entityplayer = (EntityPlayer)entityIn;
+                ItemStack itemstack = this.getHeldItemMainhand();
+                ItemStack itemstack1 = entityplayer.isHandActive() ? entityplayer.getActiveItemStack() : null;
+
+                if (itemstack != null && itemstack1 != null && itemstack.getItem() instanceof ItemAxe && itemstack1.getItem() == Items.SHIELD)
+                {
+                    float f1 = 0.25F + (float)EnchantmentHelper.getEfficiencyModifier(this) * 0.05F;
+
+                    if (this.rand.nextFloat() < f1)
+                    {
+                        entityplayer.getCooldownTracker().setCooldown(Items.SHIELD, 100);
+                        this.worldObj.setEntityState(entityplayer, (byte)30);
+                    }
+                }
+
+                entityplayer.addPotionEffect(new PotionEffect(MobEffects.POISON, 0, 30));
+                entityplayer.addPotionEffect(new PotionEffect(MobEffects.SLOWNESS, 0, 15));
+
+            }
+
+            this.applyEnchantments(this, entityIn);
+        }
+
+        return flag;
     }
 
     @Override
