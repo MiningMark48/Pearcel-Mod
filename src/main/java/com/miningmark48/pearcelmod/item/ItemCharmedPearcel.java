38fa2842a0ba7b7b785db25ff6d170a5f339fded
MiningMark48 <miningmark48@gmail.com>
2017-01-17 12:22:07 -0500

    New charm: repair

--- src/main/java/com/miningmark48/pearcelmod/item/ItemCharmedPearcel.java
+++ src/main/java/com/miningmark48/pearcelmod/item/ItemCharmedPearcel.java
@@ -13,6 +13,7 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.potion.PotionEffect;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.text.TextComponentString;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
@@ -20,6 +21,7 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 
 import javax.annotation.Nullable;
 import java.util.List;
+import java.util.Random;
 
 public class ItemCharmedPearcel extends ItemPearcelMod{
 
@@ -54,6 +56,8 @@ public class ItemCharmedPearcel extends ItemPearcelMod{
                     return 8;
                 }else if (stack.getTagCompound().getString("type").equalsIgnoreCase("hearty")){
                     return 9;
+                }else if (stack.getTagCompound().getString("type").equalsIgnoreCase("repair")){
+                    return 10;
                 }else{
                     return 0;
                 }
@@ -71,22 +75,34 @@ public class ItemCharmedPearcel extends ItemPearcelMod{
 
                 if (stack.getTagCompound().getString("type").equalsIgnoreCase("fire")){
                     list.add(TextFormatting.YELLOW + Translate.toLocal("tooltip.item.charmed_pearcel.line1.type.fire"));
+                    list.add(TextFormatting.AQUA + Translate.toLocal("tooltip.item.charmed_pearcel.line1.type.fire.desc"));
                 }else if (stack.getTagCompound().getString("type").equalsIgnoreCase("water")){
                     list.add(TextFormatting.YELLOW + Translate.toLocal("tooltip.item.charmed_pearcel.line1.type.water"));
+                    list.add(TextFormatting.AQUA + Translate.toLocal("tooltip.item.charmed_pearcel.line1.type.water.desc"));
                 }else if (stack.getTagCompound().getString("type").equalsIgnoreCase("saturation")){
                     list.add(TextFormatting.YELLOW + Translate.toLocal("tooltip.item.charmed_pearcel.line1.type.saturation"));
+                    list.add(TextFormatting.AQUA + Translate.toLocal("tooltip.item.charmed_pearcel.line1.type.saturation.desc"));
                 }else if (stack.getTagCompound().getString("type").equalsIgnoreCase("step_assist")){
                     list.add(TextFormatting.YELLOW + Translate.toLocal("tooltip.item.charmed_pearcel.line1.type.step_assist"));
+                    list.add(TextFormatting.AQUA + Translate.toLocal("tooltip.item.charmed_pearcel.line1.type.step_assist.desc"));
                 }else if (stack.getTagCompound().getString("type").equalsIgnoreCase("fall")){
                     list.add(TextFormatting.YELLOW + Translate.toLocal("tooltip.item.charmed_pearcel.line1.type.fall"));
+                    list.add(TextFormatting.AQUA + Translate.toLocal("tooltip.item.charmed_pearcel.line1.type.fall.desc"));
                 }else if (stack.getTagCompound().getString("type").equalsIgnoreCase("gravity")){
                     list.add(TextFormatting.YELLOW + Translate.toLocal("tooltip.item.charmed_pearcel.line1.type.gravity"));
+                    list.add(TextFormatting.AQUA + Translate.toLocal("tooltip.item.charmed_pearcel.line1.type.gravity.desc"));
                 }else if (stack.getTagCompound().getString("type").equalsIgnoreCase("adrenaline")){
                     list.add(TextFormatting.YELLOW + Translate.toLocal("tooltip.item.charmed_pearcel.line1.type.adrenaline"));
+                    list.add(TextFormatting.AQUA + Translate.toLocal("tooltip.item.charmed_pearcel.line1.type.adrenaline.desc"));
                 }else if (stack.getTagCompound().getString("type").equalsIgnoreCase("cloaking")){
                     list.add(TextFormatting.YELLOW + Translate.toLocal("tooltip.item.charmed_pearcel.line1.type.cloaking"));
+                    list.add(TextFormatting.AQUA + Translate.toLocal("tooltip.item.charmed_pearcel.line1.type.cloaking.desc"));
                 }else if (stack.getTagCompound().getString("type").equalsIgnoreCase("hearty")){
                     list.add(TextFormatting.YELLOW + Translate.toLocal("tooltip.item.charmed_pearcel.line1.type.hearty"));
+                    list.add(TextFormatting.AQUA + Translate.toLocal("tooltip.item.charmed_pearcel.line1.type.hearty.desc"));
+                }else if (stack.getTagCompound().getString("type").equalsIgnoreCase("repair")){
+                    list.add(TextFormatting.YELLOW + Translate.toLocal("tooltip.item.charmed_pearcel.line1.type.repair"));
+                    list.add(TextFormatting.AQUA + Translate.toLocal("tooltip.item.charmed_pearcel.line1.type.repair.desc"));
                 }else {
                     list.add(TextFormatting.RED + (Translate.toLocal("tooltip.item.charmed_pearcel.line1.no_charm")));
                 }
@@ -150,6 +166,17 @@ public class ItemCharmedPearcel extends ItemPearcelMod{
         if (stack.getTagCompound().getString("type").equalsIgnoreCase("hearty")){
             player.addPotionEffect(new PotionEffect(MobEffects.ABSORPTION, 25, 0, true, false));
             player.addPotionEffect(new PotionEffect(MobEffects.REGENERATION, 25, 0, true, false));
+        }
+        if (stack.getTagCompound().getString("type").equalsIgnoreCase("repair")){
+            if (player.getHeldItemMainhand() != null){
+                if (player.getHeldItemMainhand().getItem().isDamageable() && player.getHeldItemMainhand().getItem().isDamaged(player.getHeldItemMainhand())) {
+                    Random rand = new Random();
+                    int num = rand.nextInt(100);
+                    if (num == 0) {
+                        player.getHeldItemMainhand().getItem().setDamage(player.getHeldItemMainhand(), player.getHeldItemMainhand().getItemDamage() - 1);
+                    }
+                }
+            }
         }
     }
 
