268368ba0121cea589efa63a4c6bca5954c1868f
MiningMark48 <miningmark48@gmail.com>
2017-07-07 18:59:34 -0400

    Update time

--- src/main/java/com/miningmark48/pearcelmod/reference/Reference.java
+++ src/main/java/com/miningmark48/pearcelmod/reference/Reference.java
@@ -4,7 +4,7 @@ public class Reference {
 
     public static final String MOD_ID = "pearcelmod";
     public static final String MOD_NAME = "PearcelMod";
-    public static final String MOD_VERSION = "3.1.1";
+    public static final String MOD_VERSION = "3.1.2";
     public static final String GAME_VERSION = "1.11.2";
     public static final String VERSION = GAME_VERSION + "-" + MOD_VERSION;
     public static final String MOD_DEPENDENCIES = "required-after:mininglib";
