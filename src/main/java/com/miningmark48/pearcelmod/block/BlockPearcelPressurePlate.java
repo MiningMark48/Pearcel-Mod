187da4151564b66d921e9b68a5894eb1ab68d0ae
MiningMark48 <miningmark48@gmail.com>
2017-09-12 14:49:12 -0400

    Pressure plate bug fix

--- src/main/java/com/miningmark48/pearcelmod/block/BlockPearcelPressurePlate.java
+++ src/main/java/com/miningmark48/pearcelmod/block/BlockPearcelPressurePlate.java
@@ -1,7 +1,6 @@
 package com.miningmark48.pearcelmod.block;
 
-import com.miningmark48.pearcelmod.init.ModBlocks;
-import com.sun.javafx.binding.StringFormatter;
+import com.miningmark48.mininglib.utility.ModLogger;
 import net.minecraft.block.BlockBasePressurePlate;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.properties.PropertyBool;
@@ -11,24 +10,18 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.init.Items;
 import net.minecraft.init.SoundEvents;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.EnumParticleTypes;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
 
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
 import java.util.List;
 
 public class BlockPearcelPressurePlate extends BlockBasePressurePlate{
@@ -48,12 +41,17 @@ public class BlockPearcelPressurePlate extends BlockBasePressurePlate{
     @Override
     public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumHand hand, EnumFacing side, float hitX, float hitY, float hitZ) {
         ItemStack heldItem = playerIn.getHeldItem(hand);
-        if (heldItem != null){
+        ModLogger.info(heldItem.getUnlocalizedName());
+        if (heldItem != ItemStack.EMPTY){
             if (heldItem.getItem().equals(mode_items_item) && !worldIn.getBlockState(pos).getValue(MODE_ITEMS)){
                 worldIn.setBlockState(pos, getDefaultState().withProperty(POWERED, false).withProperty(MODE_PLAYERS, false).withProperty(MODE_ITEMS, true));
                 if (!playerIn.isCreative() && !worldIn.isRemote){
                     if (heldItem.getCount() > 0){
-                        heldItem.setCount(-1);
+                        heldItem.shrink(1);
+                        if (heldItem.getCount() == 0){
+                            heldItem = ItemStack.EMPTY;
+                            playerIn.setHeldItem(hand, heldItem);
+                        }
                     }
                 }
             }
