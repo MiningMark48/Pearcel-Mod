9d13db35bd0b9619aaa8161d870906c7ab7710f8
MiningMark48 <miningmark48@gmail.com>
2017-10-23 23:52:39 -0400

    Update

--- src/main/java/com/miningmark48/pearcelmod/reference/Reference.java
+++ src/main/java/com/miningmark48/pearcelmod/reference/Reference.java
@@ -6,7 +6,7 @@ public class Reference {
 
     public static final String MOD_ID = "pearcelmod";
     public static final String MOD_NAME = "PearcelMod";
-    public static final String MOD_VERSION = "4.1.1";
+    public static final String MOD_VERSION = "4.1.2";
     public static final String GAME_VERSION = "1.12.1";
     public static final String VERSION = GAME_VERSION + "-" + MOD_VERSION;
     public static final String MOD_DEPENDENCIES = "required-after:mininglib@[1.1.0,); " + RedstoneFlux.VERSION_GROUP;
